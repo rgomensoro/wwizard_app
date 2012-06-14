6feda6484f500147f0f22f1d969e6b37e4143c07
Rodrigo Gomensoro <rodrigo.gomensoro@gmail.com>
2012-06-13 21:34:49 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwizardApp::Application.config.secret_token = 'af0e8872c43582ba794ce1ee0db3de086912a67f1f2f3edc5cce0b1e81e8fe829216c9d92ae314fd76006fdf8cdf7559fe57bc90c59dd45c32176b2627017833'
